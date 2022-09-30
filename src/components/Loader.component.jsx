f0a9916c0fcf9621b8060f843920ac30d179abb8
Sean Caleb Ubias <snclbmnzubs@gmail.com>
2022-10-01 00:54:42 +0800

    change loader props

--- src/components/Loader.component.jsx
+++ src/components/Loader.component.jsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@chakra-ui/react";
 
 const Loader = ({ children, ...props }) => {
   return (
-    <Skeleton {...props} startColor="gray.100" endColor="gray.200" fadeDuration={0.2} speed={1}>
+    <Skeleton {...props} startColor="#d1d5db" endColor="#d1d5db" fadeDuration={0.2} speed={1}>
       {children}
     </Skeleton>
   );
